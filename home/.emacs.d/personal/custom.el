26a55ab6c23bf4ecb531b0a799193091ee0652a3
Dayton Nolan <dnolan@gmail.com>
2013-02-12 14:34:06 -0600

    Pimp my emacs

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("6687988238584bd2c0aee68dbb31c3e8d4fb32c5a648d4fff015aa59de89ed96" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e276b97977b4ac85a47b537abfd1d0f0fe3f42b958c7fb4f71abaa5a31d4f0af" "eb7f39bb25b32f79b2a1f69e7660df9338cea349784416fc9f63cbf6946a1ebe" default))))
+ '(ansi-color-names-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(ansi-term-color-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(custom-safe-themes (quote ("6687988238584bd2c0aee68dbb31c3e8d4fb32c5a648d4fff015aa59de89ed96" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e276b97977b4ac85a47b537abfd1d0f0fe3f42b958c7fb4f71abaa5a31d4f0af" "eb7f39bb25b32f79b2a1f69e7660df9338cea349784416fc9f63cbf6946a1ebe" default)))
+ '(fci-rule-color "#383838"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:height 170 :family "Monaco")))))
