c002fb2f312862c45cdcf1c9afc4d5fd49e57bc9
Dayton Nolan <daytonn@gmail.com>
2013-07-18 16:59:47 -0500

    Remove emacs dir

--- home/.emacs.d/elpa/logito-20120225.2155/logito-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; logito-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/logito-20120225.2155/logito-pkg.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/logito-20120225.2155/logito.el")
-;;;;;;  (20812 35963 910406 0))
-
-;;;***
-
-(provide 'logito-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; logito-autoloads.el ends here
--- home/.emacs.d/elpa/pcache-20120408.1206/pcache-autoloads.el
+++
@@ -1,20 +0,0 @@
-;;; pcache-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/pcache-20120408.1206/pcache-pkg.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/pcache-20120408.1206/pcache-tests.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/pcache-20120408.1206/pcache.el")
-;;;;;;  (20812 35964 757058 0))
-
-;;;***
-
-(provide 'pcache-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; pcache-autoloads.el ends here
